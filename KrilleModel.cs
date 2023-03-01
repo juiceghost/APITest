ba52b3e64215f1e6c961657c44ef6d9edfe236fc
Kristian Kjeldsen <jg@renegade.se>
2023-03-01 10:12:12 +0100

    initial commit

---
+++ KrilleModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace APITest
+{
+	public class KrilleModel
+	{
+		public int id{ get; set; }
+		public string name { get; set; }
+		public string description { get; set; }
+		public string? location { get; set; }
+	}
+}
+
