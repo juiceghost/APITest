505687e0c1f73242535f89604dd7203291fa41e7
Kristian Kjeldsen <jg@renegade.se>
2023-04-18 11:51:33 +0200

    added local/remote db connection

--- RepositoryContext.cs
+++ RepositoryContext.cs
@@ -12,7 +12,9 @@ namespace APITest
         //public DbSet<Order> Orders { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer("Server=tcp:localhost,57000;Database=dev_testdb1;User ID=SA;Password=Password123!;Trusted_Connection=False;Encrypt=False;");
+            // REMOTE:
+            // optionsBuilder.UseSqlServer("Server=tcp:dev.kjeldcon.se;Database=dev_testdb1;User ID=SA;Password=Password123!;Trusted_Connection=False;Encrypt=False;");
+            // LOCAL:
             optionsBuilder.UseSqlServer("Server=tcp:localhost,57000;Database=dev_testdb1;User ID=SA;Password=Password123!;Trusted_Connection=False;Encrypt=False;");
         }
     }
